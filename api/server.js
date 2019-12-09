e05b2cb25b840c6b5e61f3fd4a733a2ec9a8e392
vbach <vlminder@student.fullsail.edu>
2019-12-09 10:22:09 -0500

    Switching port.

--- api/server.js
+++ api/server.js
@@ -3,6 +3,6 @@ const log = require('debug')('api:logging');
 // get the express application
 const app = require('./app');
 // set the port to either the one passed from the environment variables or 4000
-const port = process.env.DATABASE_URL || 4000;
+const port = process.env.PORT || 4000;
 // spin up the server and log what port it is running on
 app.listen(port, () => log(`API listening on port ${port}!`));
